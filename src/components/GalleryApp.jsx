c679fa084b1b827674c5a4d4097c16259e53bdd7
afnank16 <afnankhantheguy@gmail.com>
2026-07-15 20:41:19 +0530

    refactor: move image data to a separate file and import it in GalleryApp

--- src/components/GalleryApp.jsx
+++ src/components/GalleryApp.jsx
@@ -1,4 +1,5 @@
 import { Search, Video } from "lucide-react";
+import {pics} from "../images/photos"
 import toast, { Toaster } from 'react-hot-toast';
 import { useState } from "react";
 import { FaRegStar } from "react-icons/fa";
@@ -6,43 +7,7 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import { IoHome } from "react-icons/io5";
 
 function GalleryApp() {
-
-    const pics = [
-        {
-            img: "https://images.pexels.com/photos/36250884/pexels-photo-36250884.jpeg",
-            tags: ["building"," river"]
-        },
-        {
-            img: "https://images.pexels.com/photos/21352832/pexels-photo-21352832.jpeg",
-            tags: ["street", "lamp", "building", "bike"]
-        },
-        {
-            img: "https://images.pexels.com/photos/8624734/pexels-photo-8624734.jpeg",
-            tags: ["river", "man", "grass", "green", "fishing"]
-        },
-        {
-            img: "https://images.pexels.com/photos/37295912/pexels-photo-37295912.jpeg",
-            tags: ["mountain", "sky", "clouds", "flowers"]
-        },
-        {
-            img: "https://images.pexels.com/photos/34744003/pexels-photo-34744003.jpeg",
-            tags: ["shop", "bicycle", "street", "bike"]
-        },
-        {
-            img: "https://images.pexels.com/photos/37298148/pexels-photo-37298148.jpeg",
-            tags: ["building", "scooter", "city"]
-        },
-        {
-            img: "https://images.pexels.com/photos/36776232/pexels-photo-36776232.jpeg",
-            tags: ["forest", "trees", "clouds", "homes"]
-        },
-        {
-            img: "https://images.pexels.com/photos/36327391/pexels-photo-36327391.jpeg",
-            tags: ["building", "river", "island", "beauty"]
-        },
-    ];
-
-
+     
     const [favorites, setFavorites] = useState([])
     const [selectedImage, setSelectedImage] = useState(null);
     const [active, setActive] = useState("home");
